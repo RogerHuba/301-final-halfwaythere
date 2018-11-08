be9872402b48966f14db5e448d1c3cca0995c6d3
Roger Huba <rogerhuba@gmail.com>
2018-11-07 16:55:24 -0800

    menu additions

--- public/app.js
+++ public/app.js
@@ -1,7 +1,18 @@
 'use strict'
 
-$('#selectLocation').on('click', saveYelpData);
+var nav = document.getElementById("topNav");
+var main = document.getElementById("main");
+var menu = document.getElementsByClassName("menuitems");
+var close = document.getElementById("closebtn")
+$(document).ready(function(){
+    $('#topNav').css('height', '48px')
+})
+
+
+
+// $('#selectLocation').on('click', saveYelpData);
 $('#currentLocationBtn').on('click', getCoords);
+// $('#hamMenu').on('click', toggleMenu);
 
 function getCoords(){
     if (navigator.geolocation) {
@@ -31,6 +42,44 @@ function grabCurrentAddress(position){
     })
 }
 
-function saveYelpData(event, req, res) {
-    event.preventDefault();
-}
+// function saveYelpData(event, req, res) {
+//     event.preventDefault();
+// }
+
+
+//default to measure if/else from
+nav.style.height = "50px";
+main.style.marginTop = "50px";
+for (let i = 0; i < menu.length; i++){
+    menu[i].style.marginTop="100px";
+};
+
+close.addEventListener("click", function() {
+  var menuIcon = close.children;
+  for (let i = 0; i < menuIcon.length; i++) {
+  menuIcon[i].classList.toggle("active");
+  }   
+});
+
+function navToggle() {	
+	//to close
+	if (nav.style.height <= "275px") {
+	nav.style.height = "50px";
+	main.style.marginTop = "1px";
+	let i = 0;
+    for (let i = 0; i < menu.length; i++) {
+	    menu[i].style.opacity="0.0";
+	    menu[i].style.marginTop="100px";
+	    };
+	} 
+	//to open
+	else if (nav.style.height <= "50px") {
+        nav.style.height = "275px";
+        main.style.marginTop = "200px";
+        let i = 0;
+        for (let i = 0; i < menu.length; i++) {
+            menu[i].style.opacity="1.0";
+            menu[i].style.marginTop="0px";
+	    };
+	}
+};
