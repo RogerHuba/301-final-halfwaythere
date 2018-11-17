99a61ea434afd6ee0ecbe04de70a1bcaac6e640e
Tanner Seramur <seramurt@gmail.com>
2018-11-17 14:03:57 -0800

    fix hamburger

--- public/nav.js
+++ public/nav.js
@@ -1,13 +1,10 @@
 $(window).on('load',function(){
-  console.log('first')
 
   $('.hamburger-nav').on('click', function(){
-    console.log('second')
 
     $('.menu').fadeToggle('slow').toggleClass('menu-hide');
     $('.menu').animate({
-      height: 'toggle'
-
+      // height: 'toggle'
     });
 
   });
